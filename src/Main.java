7d1f05374524bead885d9806f0cbe522ca46fdd0
arnoldgf <arnoldgf@mail.ru>
2023-08-22 20:45:44 +0300

    build 0.1

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введи чёта ёпта;D ");
+        System.out.print("Введите строку по примеру \"a\" + \"b\", \"a\" - \"b\", \"a\" * b, \"a\" / b : ");
         String expression = scanner.nextLine();
         String operator;
         String result = null;
+        if(!expression.contains("\"")) throw new Exception("Нужно вводить с кавычками!");
         expression = expression.replace(" ", "");
         expression = expression.replace("\"", "");
         String[] operands = expression.split("[+\\-*/]");
         operator = detectOperation(expression);
+        if(operands[1].length() > 10 || operands[1].length() < 0) throw new Exception("Чисел должно быть от 0 до 10!");
         if(operator.equals("+")){
             System.out.println(operands[0] + operands[1]);
         }
@@ -27,6 +29,7 @@ public class Main {
         }
         else if(operator.equals("*")) {
             int num = Integer.parseInt(operands[1]);
+            if(operands[1].length() < 10 && operands[1].length() > 0) throw new Exception("Чисел должно быть от 0 до 10!");
             for(int i = 0; i < num; i++){
                 System.out.print(operands[0]);
             }
@@ -34,6 +37,7 @@ public class Main {
 
         else {
             int len = operands[0].length()/Integer.parseInt(operands[1]);
+            if(operands[1].length() < 10 && operands[1].length() > 0) throw new Exception("Чисел должно быть от 0 до 10!");
             result = operands[0].substring(0, len);
             System.out.println(result);
         }
